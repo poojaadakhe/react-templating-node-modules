ffb2f9c03ff5f32bff890c8b07abaa37c578f3b3
Pooja Adakhe <pooja.adakhe@amla.io>
2020-01-31 17:39:09 +0530

    try to use js with module css

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { Dropdown } from './Components/Dropdown';
 export {List} from './Components/List';
 export {Radio} from './Components/Radio';
 export {Icon} from './Components/Icon';
+export {Tabs} from './Components/Tabs';
